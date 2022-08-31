8c567b5d5f8bd8dca4ee4a8797a8a1b0d325b35e
Mikalai Kuraka <kurakokolia@gmail.com>
2022-08-31 22:22:58 +0300

    change routing

--- chat/settings.py
+++ chat/settings.py
@@ -128,7 +128,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-ASGI_APPLICATION = 'chat.asgi.application'
+ASGI_APPLICATION = 'chat.routing.application'
 
 CHANNEL_LAYERS = {
     'default': {
