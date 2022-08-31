0cca2b84a3cdf9320cfbb24793cd7e31f7c8bb69
Mikalai Kuraka <kurakokolia@gmail.com>
2022-08-31 22:27:41 +0300

    move routing file to another folder

--- chat/asgi.py
+++ chat/asgi.py
@@ -11,7 +11,7 @@ import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-import chats.routing
+import chat.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chat.settings")
 
@@ -19,7 +19,7 @@ application = ProtocolTypeRouter({
   "http": get_asgi_application(),
   "websocket": AuthMiddlewareStack(
         URLRouter(
-            chats.routing.websocket_urlpatterns
+            chat.routing.websocket_urlpatterns
         )
     ),
 })
