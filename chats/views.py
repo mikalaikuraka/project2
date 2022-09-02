efe2996b09f5607debcd096431feb666a88d60a6
BigSmirnovsky <bigsmirnovsky@gmail.com>
2022-09-02 17:13:53 +0300

    change views for hame page

--- chats/views.py
+++ chats/views.py
@@ -7,3 +7,6 @@ def room(request, room_name):
     return render(request, 'chats/room.html', {
         'room_name': room_name
     })
+
+def home(request):
+    return render(request, 'chats/home.html')
