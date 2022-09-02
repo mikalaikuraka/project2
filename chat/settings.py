081addfb85dacc8049876e65de983617a8051dff
Mikalai Kuraka <kurakokolia@gmail.com>
2022-09-02 23:35:19 +0300

    change host

--- chat/settings.py
+++ chat/settings.py
@@ -140,7 +140,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [(os.environ.get('REDIS_LOCATION', 'https://projectchat.herokuapp.com/'), 6379)],
+            "hosts": [(os.environ.get('REDIS_LOCATION', '172.20.0.1'), 6379)],
         },
     },
 }
