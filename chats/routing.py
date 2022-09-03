c8e53a3598233cdf5bfed70b9f830ca96badd5b5
Mikalai Kuraka <kurakokolia@gmail.com>
2022-09-03 23:54:47 +0300

    change routing

--- chats/routing.py
+++ chats/routing.py
@@ -1,7 +1,9 @@
 from django.urls import re_path
+from django.urls import path
 
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'wss/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
+    path("ws/chat/<int:room_name>", consumers.ChatConsumer.as_asgi()),
+    # re_path(r'wss/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
 ]
