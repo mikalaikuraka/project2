1597c703316383facf0220718676f72d1f13451b
Mikalai Kuraka <kurakokolia@gmail.com>
2022-09-04 00:17:48 +0300

    change roting

--- chats/routing.py
+++ chats/routing.py
@@ -1,9 +1,11 @@
 from django.urls import re_path
 from django.urls import path
+from django.conf.urls import url
 
 from . import consumers
 
 websocket_urlpatterns = [
     # path("ws/chat/<int:room_name>", consumers.ChatConsumer.as_asgi()),
-    re_path(r'wss/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
+    # re_path(r'wss/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
+    url(r'^ws/chat/(?P<room_name>[^/]+)/$', consumers.ChatConsumer.as_asgi()),
 ]
