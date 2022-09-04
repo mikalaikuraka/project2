c34b8d1ac776345b8b90c9ca022d1362f90ecf3e
Mikalai Kuraka <kurakokolia@gmail.com>
2022-09-04 16:11:21 +0300

    add print to test

--- chats/consumers.py
+++ chats/consumers.py
@@ -140,7 +140,8 @@ class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
-        
+
+        print('hello')
         print(self.room_group_name)
         # # Join room group
         # async_to_sync(self.channel_layer.group_add)(
