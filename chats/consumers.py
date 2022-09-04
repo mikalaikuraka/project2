90eb1fb6acae4c33c39577d75b0baeb58284bf50
Mikalai Kuraka <kurakokolia@gmail.com>
2022-09-04 16:20:15 +0300

    add join room group

--- chats/consumers.py
+++ chats/consumers.py
@@ -141,13 +141,11 @@ class ChatConsumer(WebsocketConsumer):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
 
-        print('hello')
-        print(self.room_group_name)
-        # # Join room group
-        # async_to_sync(self.channel_layer.group_add)(
-        #     self.room_group_name,
-        #     self.channel_name
-        # )
+        # Join room group
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
 
         self.accept()
 
