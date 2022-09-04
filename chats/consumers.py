0d9c6bf5be302f8f8375f51f62b7f06746cd2949
Mikalai Kuraka <kurakokolia@gmail.com>
2022-09-04 16:08:35 +0300

    change consumers

--- chats/consumers.py
+++ chats/consumers.py
@@ -140,7 +140,8 @@ class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
-
+        
+        print(self.room_group_name)
         # # Join room group
         # async_to_sync(self.channel_layer.group_add)(
         #     self.room_group_name,
