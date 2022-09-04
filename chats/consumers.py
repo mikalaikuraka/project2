aebc97401b55d2b5e3daa71b68199fff633473a0
Mikalai Kuraka <kurakokolia@gmail.com>
2022-09-04 19:59:13 +0300

    change consumers

--- chats/consumers.py
+++ chats/consumers.py
@@ -12,17 +12,17 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         
 
-        # # Join room group
-        # await self.channel_layer.group_add(
-        #     self.room_group_name,
-        #     self.channel_name
-        # )
+        # Join room group
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
         
-        # print('принты')
-        # print(self.room_name, 'room name')
-        # print(self.room_group_name, 'group name')
-        # print(self.channel_name, 'chanel name')
-        # print(self.channel_layer, 'channel layer')
+        print('принты')
+        print(self.room_name, 'room name')
+        print(self.room_group_name, 'group name')
+        print(self.channel_name, 'chanel name')
+        print(self.channel_layer, 'channel layer')
 
         await self.accept()
 
