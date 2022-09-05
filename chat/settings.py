4a033ecfcacba0836cd104bdf30dc2ad3aed34d0
Mikalai Kuraka <kurakokolia@gmail.com>
2022-09-05 21:33:17 +0300

    cahnge setting

--- chat/settings.py
+++ chat/settings.py
@@ -41,7 +41,7 @@ else:
 INSTALLED_APPS = [
     'chats',
     'channels',
-    'bootstrap4',
+    'bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
