846a65271899defd332d4224d72ff0405abc3d98
siarheismirnou <smirnovsergei1377@gmail.com>
2022-09-07 13:17:19 +0300

    refactor consumers

--- chats/consumers.py
+++ chats/consumers.py
@@ -2,11 +2,13 @@ import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 class ChatConsumer(AsyncWebsocketConsumer):
+    """Потребитель"""
+    
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
 
-        # Join room group
+        # Создаёт и присоединяется к группе
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
@@ -15,18 +17,18 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.accept()
 
     async def disconnect(self, close_code):
-        # Leave room group
+        # Выход из группы
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
 
-    # Receive message from WebSocket
+    # Получает сообщение от WebSocket
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
-        # Send message to room group
+        # Отправляет сообщение в группу
         await self.channel_layer.group_send(
             self.room_group_name,
             {
@@ -35,11 +37,11 @@ class ChatConsumer(AsyncWebsocketConsumer):
             }
         )
 
-    # Receive message from room group
+    # Получает сообщение от группы
     async def chat_message(self, event):
         message = event['message']
 
-        # Send message to WebSocket
+        # Отправляет сообщение в WebSocket
         await self.send(text_data=json.dumps({
             'message': message
         }))
