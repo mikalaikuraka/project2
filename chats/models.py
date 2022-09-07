0924974abae6b5c8843f5ed9ceb711901d54b942
siarheismirnou <smirnovsergei1377@gmail.com>
2022-09-07 13:20:04 +0300

    refactor models

--- chats/models.py
+++ chats/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-#from django.contrib.auth.models import User
-
 
 class Message(models.Model):
+    """Модель сообщения"""
+
     id = models.AutoField(primary_key=True, unique=True)
     text = models.CharField(max_length=255)
