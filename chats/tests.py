6e83f32618523a2696f5508e610803a45a25ca7b
siarheismirnou <smirnovsergei1377@gmail.com>
2022-09-07 11:49:20 +0300

    add 3 test TestCase

---
+++ chats/tests.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+
+class SearchFormTestCase(TestCase):
+    """Tests host and page"""
+
+    def test_chat_room_status_code(self):
+        response = self.client.get('/chat/1/')
+        self.assertEquals(response.status_code, 200)
+
+    def test_chat_page_status_code(self):
+        response = self.client.get('/chat/')
+        self.assertEquals(response.status_code, 200)
+
+    def test_chat_home_status_code(self):
+        response = self.client.get('')
+        self.assertEquals(response.status_code, 200)
